52d6854e2c4c040055cc228e65123039c6c9a121
Subhasis sahoo <76907034+subhasissahoo181@users.noreply.github.com>
2024-01-18 14:25:02 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API = axios.create({ baseURL: "https://api.stackexchange.com/"})
+const API = axios.create({ baseURL:"https://api.stackexchange.com/"})
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("Profile")) {
